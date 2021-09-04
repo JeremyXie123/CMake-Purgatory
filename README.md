1b7829af1ab3c0ed8778e3bdd3df1280e7fd60ae
JeremyXie123 <jeremy.xie@tdsb.on.ca>
2021-09-04 11:57:40 -0400

    Update main to handle GLFW Code (first working version)

--- README.md
+++ README.md
@@ -8,7 +8,9 @@
 
 ## What is CMake?
 * Cmake is a system that works with make. As projects get larger with more dependencies, the makefiles can become convoluted and messy. CMake utilizes clear and concise instructions to work on top of this, with simplified commands that allow us to "build" a makefile ourselves.
+    * Options within the CMakeLists of libraries can be enabled via `-D<option>=<on/off>`
 
+    * Make sure to always build your project after configuring it, otherwise the `lib<name>.a` file won't be generated
 # Extra Info
 * CMake tutorial I followed when making this repository: https://www.youtube.com/watch?v=nlKcXPUJGwA
 * I have not yet tried other C++ dependency managers such as vcpkg, but I may in the future.
--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 #include "Adder/adder.h" // Include the header so the program knows add is a function that will be defined before it's called
+#include <GLFW/glfw3.h>
 
 using namespace std;
 int main() {
     cout << "Hello World!" << endl;
     cout << add(72.1 , 73.8) << endl;
+
+    GLFWwindow *window;
+    
+    if (!glfwInit()) {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window) {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Draw gears
+        // draw();
+
+        // Update animation
+        // animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
     return 0;
 }
